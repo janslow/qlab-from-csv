a73db05d95ae0558140d01c0958e6b7aa53e60b6
Jay Anslow <jay@anslow.me.uk>
2014-12-20 22:51:10 +0000

    Added StartCue

---
+++ QLab From CSV/QLab From CSV/start_cue.swift
@@ -0,0 +1,32 @@
+//
+//  start_cue.swift
+//  QLab From CSV
+//
+//  Created by Jay Anslow on 20/12/2014.
+//  Copyright (c) 2014 Jay Anslow. All rights reserved.
+//
+
+import Foundation
+
+class StartCue : Cue {
+    var cueNumber : String? {
+        return nil
+    }
+    var cueName : String {
+        return "Start \(targetNumber)"
+    }
+    var cueShortName : String {
+        return targetNumber
+    }
+    var preWait : Float
+    var targetNumber : String
+    
+    init(targetNumber : String, preWait : Float = 0) {
+        self.targetNumber = targetNumber
+        self.preWait = preWait
+    }
+    
+    func create() {
+        // TODO
+    }
+}
