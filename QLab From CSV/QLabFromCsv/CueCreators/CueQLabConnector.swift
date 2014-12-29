d8b4245e1b7a07fc444908ccf5fa4e75257931d9
Jay Anslow <jay@anslow.me.uk>
2014-12-29 08:37:12 +0000

    Added CueQLabConnector
    Appends Cues of any type to QLab workspace.

---
+++ QLab From CSV/QLabFromCsv/CueCreators/CueQLabConnector.swift
@@ -0,0 +1,36 @@
+//
+//  CueCreator.swift
+//  QLab From CSV
+//
+//  Created by Jay Anslow on 28/12/2014.
+//  Copyright (c) 2014 Jay Anslow. All rights reserved.
+//
+
+import Foundation
+
+class CueQLabConnector {
+    private let _workspace : QLKWorkspace
+    
+    init(workspace : QLKWorkspace) {
+        _workspace = workspace
+    }
+    func appendCues(var cues : [Cue], completion : () -> ()) {
+        if (cues.isEmpty) {
+            completion()
+        }
+        var remainingCues = cues
+        var nextCue = cues.removeAtIndex(0)
+        appendCue(nextCue, {
+            self.appendCues(remainingCues, completion)
+        })
+    }
+    func appendCue(cue : Cue, completion : () -> ()) {
+        // TODO: Handle cues correctly
+        _workspace.sendMessage("group", toAddress:"/new", block: {
+            (data : AnyObject!) in
+            let uid = data as String
+            println("Created cue with UID=\(uid)")
+            completion()
+        })
+    }
+}
