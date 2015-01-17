3a83a32e07f9b37ddac84378a4fd29bbaf6f44fc
Jay Anslow <jay@anslow.me.uk>
2015-01-17 17:58:09 +0000

    QLab cues now have preWait set.
    Also reorganised CueQLabConnectorBase structure.

--- QLab From CSV/QLabFromCsv/CueQLabConnectorBase.swift
+++ QLab From CSV/QLabFromCsv/CueQLabConnectorBase.swift
@@ -21,17 +21,47 @@ class CueQLabConnectorBase {
             (data : AnyObject!) in
             let uid = data as String
             println("INSERT \(cueType) cue RESPONSE uid = \(uid)")
-            let number = cue.cueNumber ?? ""
-            self._workspace.sendMessage(number, toAddress:"/cue_id/\(uid)/number") {
-                (data : AnyObject!) in
-                println("UPDATE cue.number = \(number) WHERE cue.uid = \(uid) RESPONSE \(data)")
-                let name = cue.cueName ?? ""
-                self._workspace.sendMessage(name, toAddress:"/cue_id/\(uid)/name") {
-                    (data : AnyObject!) in
-                    println("UPDATE cue.name = \(name) WHERE cue.uid = \(uid) RESPONSE \(data)")
-                    completion(uid: uid)
+            
+            self.updateNumber(uid, number: cue.cueNumber) {
+                self.updateName(uid, name: cue.cueName) {
+                    self.updatePreWait(uid, preWait: cue.preWait) {
+                        completion(uid: uid)
+                    }
                 }
             }
         })
+    }
+    
+    private func updateNumber(uid : String, number : String?, completion : () -> ()) {
+        let numberNonNull = number ?? ""
+        self._workspace.sendMessage(numberNonNull, toAddress:"/cue_id/\(uid)/number") {
+            (data : AnyObject!) in
+            println("UPDATE cue.number = \"\(numberNonNull)\" WHERE cue.uid = \(uid) RESPONSE \(data)")
+            completion()
+        }
+    }
+    
+    private func updateName(uid : String, name : String?, completion : () -> ()) {
+        if let nameNonNull = name {
+            self._workspace.sendMessage(nameNonNull, toAddress:"/cue_id/\(uid)/name") {
+                (data : AnyObject!) in
+                println("UPDATE cue.name = \"\(nameNonNull)\" WHERE cue.uid = \(uid) RESPONSE \(data)")
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+    
+    private func updatePreWait(uid : String, preWait : Float, completion : () -> ()) {
+        if preWait > 0.0 {
+            self._workspace.sendMessage(preWait, toAddress:"/cue_id/\(uid)/preWait") {
+                (data : AnyObject!) in
+                println("UPDATE cue.preWait = \(preWait) WHERE cue.uid = \(uid) RESPONSE \(data)")
+                completion()
+            }
+        } else {
+            completion()
+        }
     }
 }
