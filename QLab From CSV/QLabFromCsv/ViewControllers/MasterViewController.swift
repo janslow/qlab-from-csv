06f57cf7ab246406202851eeb57a658e1fc96988
Jay Anslow <jay@anslow.me.uk>
2015-04-21 22:42:58 +0100

    Moved ViewController impls into their own files.

--- QLab From CSV/QLabFromCsv/ViewControllers/MasterViewController.swift
+++ QLab From CSV/QLabFromCsv/ViewControllers/MasterViewController.swift
@@ -8,61 +8,10 @@
 
 import Foundation
 
-@objc public protocol MasterViewController {
+public protocol MasterViewController {
     var QLabController : QLabViewController { get }
     var CuesController : CuesViewController { get }
     
     func fireCheckValid()
     func append()
-}
-
-@objc public class MasterViewControllerImpl : NSTabViewController, MasterViewController {
-    @IBOutlet weak var qlabTab: NSTabViewItem!
-    @IBOutlet weak var cuesTab: NSTabViewItem!
-    @IBOutlet weak var executeTab: NSTabViewItem!
-    
-    required public init?(coder : NSCoder) {
-        super.init(coder: coder)
-        MAIN_VIEW_CONTROLLER = self
-    }
-    
-    public var QLabController : QLabViewController {
-        get {
-            return qlabTab.viewController! as QLabViewController
-        }
-    }
-    public var CuesController : CuesViewController {
-        get {
-            return cuesTab.viewController! as CuesViewController
-        }
-    }
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        log.debug("Loaded MasterViewController")
-        QLabController.Parent = self
-        CuesController.Parent = self
-    }
-    
-    public override var representedObject: AnyObject? {
-        didSet {
-            // Update the view, if already loaded.
-        }
-    }
-    
-    public func fireCheckValid() {
-        APP_DELEGATE?.setIsRunAllowed(QLabController.IsConnected && CuesController.IsValid)
-    }
-    
-    public func append() {
-        if QLabController.IsConnected && CuesController.IsValid {
-            let connector = CueQLabConnector(workspace: QLabController.Workspace!)
-            log.info("Appending cues.")
-            connector.appendCues(CuesController.Cues) {
-                (uids : [String]) in
-                log.info("Appended \(uids.count) cues.")
-            }
-        }
-    }
-    
 }
