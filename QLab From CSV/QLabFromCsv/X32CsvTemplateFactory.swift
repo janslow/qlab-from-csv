3c922cb936d3abb547867a61b697ce741ba6be8a
Jay Anslow <jay@anslow.me.uk>
2015-04-25 21:51:29 +0100

    Updated X32CsvTemplateFactory
    Template now creates all required cues.

--- QLab From CSV/QLabFromCsv/X32CsvTemplateFactory.swift
+++ QLab From CSV/QLabFromCsv/X32CsvTemplateFactory.swift
@@ -58,6 +58,17 @@ public class X32CsvTemplateFactory {
             break
         }
         
+        if columnName.hasPrefix("VCA") || columnName.hasPrefix("DCA") {
+            var dcaString = columnName.substringFromIndex(advance(columnName.startIndex, 3))
+            dcaString = dcaString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+            if let dca = dcaString.toInt() {
+                return buildDCACueParser(dca)
+            } else {
+                issues.add(IssueSeverity.ERROR, line: 1, cause: columnName, code: "INVALID_DCA_COLUMN_NAME", details: "Unable to parse DCA number from column name")
+                return nil
+            }
+        }
+        
         issues.add(IssueSeverity.WARN, line: 1, cause: columnName, code: "UNKNOWN_COLUMN_NAME", details: "Unable to create CueParser for column.")
         return nil
     }
@@ -66,7 +77,7 @@ public class X32CsvTemplateFactory {
         return {
             (parts : [String], preWait : Float, issues : ParseIssueAcceptor, line : Int) -> [Cue] in
             if parts.count < 1 {
-                issues.add(IssueSeverity.ERROR, line: line, cause: nil, code: "MISSING_CHANNEL", details: "The channel number to mute/unassign is missing")
+                issues.add(IssueSeverity.ERROR, line: line, cause: nil, code: "MISSING_PARAMETERS", details: "The channel number to mute/unassign is missing")
                 return []
             }
             if parts.count > 1 {
@@ -82,5 +93,65 @@ public class X32CsvTemplateFactory {
                 return []
             }
         }
+    }
+    
+    private static func buildDCACueParser(dca : Int) -> CueParser {
+        return {
+            (parts : [String], preWait : Float, issues : ParseIssueAcceptor, line : Int) -> [Cue] in
+            if parts.count < 1 {
+                issues.add(IssueSeverity.ERROR, line: line, cause: nil, code: "MISSING_PARAMETERS", details: "The DCA name is missing")
+                return []
+            }
+            if parts[0] == "*" {
+                return self.parseInactiveDCACue(dca, parts: parts, preWait: preWait, issues: issues, line: line)
+            } else {
+                return self.parseActiveDCACue(dca, parts: parts, preWait: preWait, issues: issues, line: line)
+            }
+        }
+    }
+    
+    private static func parseInactiveDCACue(dca : Int, parts : [String], preWait : Float, issues : ParseIssueAcceptor, line : Int) -> [Cue] {
+        if parts.count > 1 {
+            issues.add(IssueSeverity.WARN, line: line, cause: "\(parts)", code: "EXTRA_PARAMETERS", details: "Only the DCA name was expected")
+        }
+        let cues : [Cue] = [
+            X32SetDCANameCue(dca: dca, name: "", preWait: preWait),
+            X32SetDCAColourCue(dca: dca, colour: X32Colour.OFF, preWait: preWait)
+        ]
+        let disableDCACue : Cue = GroupCue(cueNumber: "", comment: "Disable DCA \(dca)", page: nil, children: cues)
+        return [disableDCACue]
+    }
+    
+    private static func parseActiveDCACue(dca : Int, parts : [String], preWait : Float, issues : ParseIssueAcceptor, line : Int) -> [Cue] {
+        if parts.count < 2 {
+            issues.add(IssueSeverity.ERROR, line: line, cause: nil, code: "MISSING_PARAMETERS", details: "The DCA name and channel numbers are missing")
+            return []
+        }
+        if parts.count > 2 {
+            issues.add(IssueSeverity.WARN, line: line, cause: "\(parts)", code: "EXTRA_PARAMETERS", details: "Only the DCA name and channel numbers were expected")
+        }
+        let name = parts[0]
+        let channels : [Int] = parts[1].componentsSeparatedByString("+").map({
+            $0.toInt()
+        }).filter({
+            (channel : Int?) -> Bool in
+            if (channel != nil) {
+                issues.add(IssueSeverity.ERROR, line: line, cause: "\(parts)", code: "INVALID_DCA_CHANNELS", details: "Channel numbers to must be integers")
+                return true
+            }
+            return false
+        }).map({
+            $0!
+        })
+        var cues : [Cue] = [
+            X32SetDCANameCue(dca: dca, name: name, preWait: preWait),
+            X32SetDCAColourCue(dca: dca, colour: X32Colour.BLUE, preWait: preWait)
+        ]
+        for channel in channels {
+            cues.append(X32AssignChannelToDCACue(channel: channel, dca: dca, preWait: preWait))
+            cues.append(X32SetChannelMixOnCue(channel: channel, on: true, preWait: preWait))
+        }
+        let enableDCACue : Cue = GroupCue(cueNumber: "", comment: "Enable DCA \(dca) - \(name)", page: nil, children: cues)
+        return [enableDCACue]
     }
 }
