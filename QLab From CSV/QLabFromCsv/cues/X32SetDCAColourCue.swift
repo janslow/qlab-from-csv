25f4bbdbde4882be88067543fe9e31eed5ec7026
Jay Anslow <jay@anslow.me.uk>
2015-04-25 20:37:57 +0100

    Added X32 cues for setting name and colour of DCAs.

---
+++ QLab From CSV/QLabFromCsv/cues/X32SetDCAColourCue.swift
@@ -0,0 +1,27 @@
+
+//
+//  X32SetDCAColourCue.swift
+//  QLab From CSV
+//
+//  Created by Jay Anslow on 2015-04-25.
+//  Copyright (c) 2015 Jay Anslow. All rights reserved.
+//
+
+import Foundation
+
+class X32SetDCAColourCue : X32DCACue {
+    let colour : X32Colour
+    
+    override var cueName : String {
+        return "X32 => DCA\(dca) => colour \(colour.rawValue)"
+    }
+    
+    override var description : String {
+        return "X32DCA\(dca)Colour"
+    }
+    
+    init(dca: Int, colour: X32Colour, preWait: Float) {
+        self.colour = colour
+        super.init(dca: dca, dcaOscString: "config/color \(colour.rawValue)", preWait: preWait)
+    }
+}
